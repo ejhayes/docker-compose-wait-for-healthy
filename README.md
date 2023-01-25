728c1aac9f9c676f946ca1ed6b7608f4330396b1
Eric Hayes <eric@deployfx.com>
2023-01-24 16:57:44 -0800

    v1 (#1)
    V1 initial release

---
+++ README.md
@@ -0,0 +1,68 @@
+# docker compose wait for healthy
+
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+
+[![All Contributors](https://img.shields.io/badge/all_contributors-0-orange.svg?style=flat-square)](#contributors-)
+
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+<!-- action-docs-description -->
+
+## Description
+
+This action waits for a docker compose services to be marked as healthy
+
+<!-- action-docs-description -->
+
+_IMPORTANT_: in order for this to work your container must have a functioning HEALTHCHECK directive. More info on that can be found [here](https://docs.docker.com/engine/reference/builder/#healthcheck).
+
+## Usage
+
+To use this in your projects do this:
+
+```
+...
+- uses: ejhayes/docker-compose-wait-for-healthy@releases/main
+  with:
+    services: 'space separate list of services you want to wait for'
+    path: relativePathToDockerComposeFile
+...
+```
+
+<!-- action-docs-inputs -->
+
+## Inputs
+
+| parameter | description                                      | required | default                 |
+| --------- | ------------------------------------------------ | -------- | ----------------------- |
+| timeout   | amount of time in seconds to wait before failing | `false`  | 60                      |
+| services  | space separate list of services to wait for      | `false`  |                         |
+| path      | default path to use                              | `false`  | ${{ github.workspace }} |
+
+<!-- action-docs-inputs -->
+
+<!-- action-docs-outputs -->
+
+<!-- action-docs-outputs -->
+
+<!-- action-docs-runs -->
+
+## Runs
+
+This action is a `node16` action.
+
+<!-- action-docs-runs -->
+
+## Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import 'source-map-support/register';
+
+import { exec, spawn } from 'child_process';
+import { promisify } from 'util';
+import { error, getInput, info } from '@actions/core';
+
+const INPUTS = {
+  services: getInput('services', { required: false }),
+  path: getInput('path', { required: false }),
+  timeout: Number(getInput('timeout', { required: false })),
+};
+
+async function main() {
+  return new Promise<void>((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject('Timeout');
+      return;
+    }, INPUTS.timeout * 1000);
+
+    const intervalId = setInterval(async () => {
+      const services = await getServices(INPUTS.services, INPUTS.path);
+      if (services.length == services.filter((i) => i[1]).length) {
+        info('Status: All services HEALTHY');
+        clearInterval(intervalId);
+        clearTimeout(timeoutId);
+        resolve();
+      } else {
+        info(
+          `Status: ${services.filter((i) => i[1]).length}/${
+            services.length
+          } services are HEALTHY`,
+        );
+      }
+    }, 1000);
+  });
+}
+
+async function getServices(services, cwd) {
+  const cmd = await runCommand(`docker compose ps ${services}`, cwd);
+
+  // find indexes we care about
+  const res = cmd.trim().split('\n');
+  const headers = res[0].split(/\s{3,}/);
+
+  return res
+    .slice(1)
+    .map((i) => i.split(/\s{3,}/))
+    .map((i) => [i[headers.indexOf('SERVICE')], i[headers.indexOf('STATUS')]])
+    .map((i) => [i[0], i[1].search(/healthy/i) >= 0]);
+}
+
+function streamCommand(command, args, cwd) {
+  const cmd = spawn(command, args, {
+    detached: true,
+    cwd,
+    stdio: ['pipe', process.stdout, process.stderr],
+  });
+
+  return cmd;
+}
+
+async function runCommand(command, cwd) {
+  try {
+    const { stdout } = await promisify(exec)(command, { cwd });
+    return `${stdout}`;
+  } catch (err) {
+    throw err;
+  }
+}
+
+const cmd = streamCommand(
+  'docker',
+  ['compose', 'logs', '-f', INPUTS.services],
+  INPUTS.path,
+);
+main()
+  .then(() => {
+    // need to make sure all spawned processes are killed
+    process.kill(-cmd.pid);
+  })
+  .catch((err) => {
+    error(err);
+    process.kill(-cmd.pid);
+    process.exit(1);
+  });
